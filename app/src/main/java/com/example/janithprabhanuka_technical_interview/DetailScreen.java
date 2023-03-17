12ea9f2494efcfb52d0021a8ab1f2adebc5ae316
Janith <janithprabhanuka95@gmail.com>
2023-03-17 23:34:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/janithprabhanuka_technical_interview/DetailScreen.java
@@ -0,0 +1,13 @@
+package com.example.janithprabhanuka_technical_interview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_screen);
+    }
+}
---
+++ app/src/main/java/com/example/janithprabhanuka_technical_interview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.janithprabhanuka_technical_interview;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button searchbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
